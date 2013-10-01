196e37198b3803fcad351bd0cd2404dd380468a3
Michael Lasky <mlasky@Michaels-Mac-Pro.local>
2013-09-30 21:46:29 -0400

    Added angular

--- www/js/lib/init.js
+++ www/js/lib/init.js
@@ -2,7 +2,8 @@
     requirejs.config({
         baseUrl: 'js/lib',
         paths: {
-            jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+            jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+            angular: "https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min"
         },
 
         map: {
@@ -13,6 +14,6 @@
     });
 })();
 
-require(['jquery'], function($) {
+require(['jquery', 'angular'], function($) {
     console.log($);
 });
