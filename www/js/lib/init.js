cf793e0a09fe12e17974f7113f43ddc19bd0f896
Michael Lasky <michaellasky@mabosdkramer-m1.local>
2013-10-01 21:21:58 -0400

    Problem was being caused by Evernote Chrome Extension interfering with require.js

--- www/js/lib/init.js
+++ www/js/lib/init.js
@@ -3,7 +3,7 @@
         baseUrl: 'js/lib',
         paths: {
             jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-            //angular: "https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min"
+            angular: "https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min"
         },
 
         map: {
@@ -13,7 +13,7 @@
         }
     });
 
-    require(['jquery'], function($) {
+    require(['jquery', 'angular'], function($) {
         console.log($);
     });
 })();
