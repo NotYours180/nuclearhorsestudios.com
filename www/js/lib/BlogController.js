20153362332d8aa10e2883d8fbe43ffcbff8c54b
Michael Lasky <mlasky@Michaels-Mac-Pro.local>
2013-10-05 16:21:38 -0400

    Inlined reqirejs

---
+++ www/js/lib/BlogController.js
@@ -0,0 +1,5 @@
+define(['jquery', 'angular', 'underscore'], function($, angular) {
+    return function BlogController ($scope) {
+        $scope.hello = 'hi';
+    }
+});
--- www/js/lib/init.js
+++ www/js/lib/init.js
@@ -1,30 +1,19 @@
 (function() {
-    requirejs.config({
-        baseUrl: 'js/lib',
-        paths: {
-            jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
-            angular: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min',
-            underscore: 'dep/underscore.min'
-        },
-        shim: {
-            'angular': { 
-                deps: ['jquery'],
-                exports: 'angular'
-            },
-            'underscore': {
-                exports: '_'
+    require(['jquery', 'angular', 'underscore', 'BlogController'], function($, angular) {
+        var nhs_com = angular.module('NuclearHorseStudios_WWW', []);
+
+        nhs_com.config([
+            '$routeProvider', 
+            function($routeProvider) {
+                $routeProvider
+                    .when('/blog', {
+                        templateUrl: 'partials/blog.html', 
+                        controller: BlogController
+                    })
+                    .otherwise({redirectTo: '/blog'});
             }
-        },
-        map: {
-            '*': { 
-                jquery: 'dep/jqueryPrivate',
-            },
+        ]);
 
-            'dep/jqueryPrivate':  { jquery:  'jquery'  }
-        }
-    });
-
-    require(['jquery', 'angular', 'underscore'], function($, angular) {
-        console.log(_);
+        console.log('init')
     });
 })();
