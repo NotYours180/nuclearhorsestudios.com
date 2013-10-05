8cff6a1216d48301a9960679069ac34a3326c392
Michael Lasky <mlasky@Michaels-Mac-Pro.local>
2013-10-05 14:08:02 -0400

    Added underscore.js

--- www/js/lib/init.js
+++ www/js/lib/init.js
@@ -2,13 +2,17 @@
     requirejs.config({
         baseUrl: 'js/lib',
         paths: {
-            jquery: "https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-            angular: "https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min"
+            jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+            angular: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min',
+            underscore: 'dep/underscore.min'
         },
         shim: {
             'angular': { 
                 deps: ['jquery'],
                 exports: 'angular'
+            },
+            'underscore': {
+                exports: '_'
             }
         },
         map: {
@@ -20,7 +24,7 @@
         }
     });
 
-    require(['jquery', 'angular'], function($, angular) {
-        console.log(angular);
+    require(['jquery', 'angular', 'underscore'], function($, angular) {
+        console.log(_);
     });
 })();
