6b09a5155638d6de8083736469864e1dc94757d1
Michael Lasky <mlasky@Michaels-Mac-Pro.local>
2013-10-07 16:25:13 -0400

    Added jasmine test framework

--- www/js/lib/NuclearHorseStudios/NuclearHorseStudios.js
+++ www/js/lib/NuclearHorseStudios/NuclearHorseStudios.js
@@ -1,42 +1,3 @@
-
-requirejs.config({
-    baseUrl: '../js/lib',
-    paths: {
-        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
-        angular: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular.min',
-        ngResource: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-resource.min',
-        ngRoute: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0rc1/angular-route.min',
-        underscore: 'dep/underscore.min',
-        BlogController: 'NuclearHorseStudios/BlogController',
-        CreationsController: 'NuclearHorseStudios/CreationsController',
-        ContactController: 'NuclearHorseStudios/ContactController'
-    },
-    shim: {
-        'angular': { 
-            deps: ['jquery'],
-            exports: 'angular'
-        },
-        'ngResource': {
-            deps: ['angular'],
-            exports: 'ngResource'
-        },
-        'ngRoute': {
-            deps: ['angular'],
-            exports: 'ngRoute',
-        },
-        'underscore': {
-            exports: '_'
-        }
-    },
-    map: {
-        '*': { 
-            jquery: 'dep/jqueryPrivate',
-        },
-
-        'dep/jqueryPrivate':  { jquery:  'jquery'  }
-    }
-});
-
 define([
     'jquery', 
     'angular', 
@@ -67,19 +28,17 @@ define([
                         templateUrl: 'partials/blog.html', 
                         controller: BlogController
                     })
-                     .when('/creations', {
+                    .when('/creations', {
                         templateUrl: 'partials/creations.html', 
                         controller: CreationsController
                     })
-                      .when('/contact', {
+                    .when('/contact', {
                         templateUrl: 'partials/contact.html', 
                         controller: ContactController
                     })
                     .otherwise({redirectTo: '/blog'});
             }
         ]);
-
-        nhs.controller('BlogController', BlogController);
         
         angular.bootstrap(document, ['NuclearHorseStudios']);
     
---
+++ www/js/lib/NuclearHorseStudios/tests/spec/BlogControllerSpec.js
@@ -0,0 +1,17 @@
+define([
+    'jquery', 
+    'angular', 
+    'ngResource', 
+    'ngRoute', 
+    'BlogController',
+    'jasmine',
+    'underscore'],
+
+    function($, angular, ngResource, ngRoute, BlogController) {
+        describe("This is a test", function() {
+            it('should be true', function() {
+                expect(true).toBe(true);
+            });
+        });
+    }
+);
