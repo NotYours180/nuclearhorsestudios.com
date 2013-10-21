b35dc336fe648a8956e96859e5923b02bfe7637a
Michael Lasky <lasky.michael@gmail.com>
2013-10-21 03:16:28 -0400

    Sunday night commit

--- www/js/lib/nuclearHorseStudios/controllers/blogPostsPaginatedController.js
+++ www/js/lib/nuclearHorseStudios/controllers/blogPostsPaginatedController.js
@@ -1,21 +1,30 @@
 define(['underscore'], function() {
+    'use strict';
+
     return function($scope, blogData) {
 
-        var _requestedPage;
+        var _requestedPage,
+            _requestedItemsPerPage;
 
-        $scope.currentPage = 1;
+        $scope.currentPage  = 1;
+        $scope.itemsPerPage = 5;
+        $scope.totalPosts   = 0;
 
         $scope.onGetPage = function(data, status) {
             $scope.posts = _(data.rows).map(function(row) {
                 return row.value;
             });
 
-            $scope.currentPage = _requestedPage;
+            $scope.currentPage  = _requestedPage;
+            $scope.itemsPerPage = _requestedItemsPerPage;
+            $scope.totalPosts   = data.total_rows;
+            $scope.numPages     = parseInt($scope.totalPosts / $scope.itemsPerPage, 10);
         };
 
         $scope.getPage = function(pageNum, itemsPerPage) {
-            _requestedPage = pageNum;
-            
+            _requestedPage          = pageNum;
+            _requestedItemsPerPage  = itemsPerPage;
+
             return blogData.getPage(pageNum, itemsPerPage)
                            .success($scope.onGetPage);
         };
--- www/js/lib/nuclearHorseStudios/tests/spec/blogPostsPaginatedControllerSpec.js
+++ www/js/lib/nuclearHorseStudios/tests/spec/blogPostsPaginatedControllerSpec.js
@@ -1,27 +1,39 @@
 define(['Controllers', 'MockHttp'], function(controllers, mockHttp) {
-    var controller, blogData, $scope;
+    'use strict';
+
+    var controller;
+    var blogData;
+    var $scope = {};
+    var postData;
 
     describe('BlogPostsPaginatedController', function() {
         beforeEach(function() {
+            postData = {
+                offset: 0,
+                rows: [
+                    { value: { title: 'Title 1'  } }, 
+                    { value: { title: 'Title 2'  } },
+                    { value: { title: 'Title 3'  } },
+                    { value: { title: 'Title 4'  } },
+                    { value: { title: 'Title 5'  } }, 
+                    { value: { title: 'Title 6'  } }, 
+                    { value: { title: 'Title 7'  } },
+                    { value: { title: 'Title 8'  } },
+                    { value: { title: 'Title 9'  } },
+                    { value: { title: 'Title 10' } }
+                ]
+            };
+
             blogData = {
                 getPage: function(pageNum, itemsPerPage) {
-                    var pageData = {
-                        rows: [
-                            { value: { title: 'Title 1'  } }, 
-                            { value: { title: 'Title 2'  } },
-                            { value: { title: 'Title 3'  } },
-                            { value: { title: 'Title 4'  } },
-                            { value: { title: 'Title 5'  } }, 
-                            { value: { title: 'Title 6'  } }, 
-                            { value: { title: 'Title 7'  } },
-                            { value: { title: 'Title 8'  } },
-                            { value: { title: 'Title 9'  } },
-                            { value: { title: 'Title 10' } }
-                        ]
-                    };
                     var offset = (pageNum - 1) * itemsPerPage;
 
-                    mockHttp.data = { rows: pageData.rows.slice(offset, offset + itemsPerPage) };
+                    mockHttp.data = { 
+                        total_rows: 10,
+                        offset: offset,
+                        rows: postData.rows.slice(offset, offset + itemsPerPage) 
+                    };
+                    
                     return mockHttp;
                 }
             };
@@ -63,6 +75,22 @@ define(['Controllers', 'MockHttp'], function(controllers, mockHttp) {
                 $scope.getPage(2, 3);
                 
                 expect($scope.currentPage).toBe(2);                
+            });
+
+            it('Sets $scope.itemsPerPage to 3 upon successfully getting page 2, 3 results per page.', function() {
+                $scope.getPage(2, 3);
+                
+                expect($scope.itemsPerPage).toBe(3);                
+            });
+
+            it('Sets $scope.totalPosts to 10 with mock 10 rows returned', function() {
+                $scope.getPage(1, 5);
+                expect($scope.totalPosts).toBe(10);
+            });
+
+            it('Sets $scope.numPages to 5 with mock request 2 posts per page', function() {
+                $scope.getPage(1, 2);
+                expect($scope.numPages).toBe(5);
             });
         });
     });
